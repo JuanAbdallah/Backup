0fd9eed66da33095c3a54a8e3e67913ac9ded897
Juan Abdallah Ritti de Oliveira <141188485+JuanAbdallah@users.noreply.github.com>
2024-09-07 19:30:26 -0300

    Delete estacionamento-main/estacionamento-main directory

--- estacionamento-main/estacionamento-main/Code/src/main/java/br/vianna/estacionamento/model/e/ETipoPlano.java
+++
@@ -1,5 +0,0 @@
-package br.vianna.estacionamento.model.e;
-
-public enum ETipoPlano {
-    MENSAL, SEMANAL, DIARIA, HORA;
-}
--- estacionamento-main/estacionamento-main/Code/src/main/java/br/vianna/estacionamento/model/e/ETipoUsuario.java
+++
@@ -1,5 +0,0 @@
-package br.vianna.estacionamento.model.e;
-
-public enum ETipoUsuario {
-    FUNCIONARIO, CLIENTE;
-}
